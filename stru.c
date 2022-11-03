19a006ff89c8b400b1c25e8f26772a703c8978b6
Naveenalla3000 <111577927+Naveenalla3000@users.noreply.github.com>
2022-11-03 08:09:58 +0530

    Update stru.c

--- stru.c
+++ stru.c
@@ -9,14 +9,14 @@ struct cricket
 };
 
 
-void main()
+main()
 {
-	struct cricket cri[100],tem[99];
+	struct cricket cri[100],tem;
 	int n,i;
 	printf("Enter the no.of players : ");
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
-	printf("Enter the name, runs,centuries,rank and steic rate of %d: ",i+1);
+	printf("Enter the name, runs,centuries,rank and steic rate of %d: ",i);
 	scanf("%s%d%d%d%f",cri[i].name,&cri[i].runs,&cri[i].cen,&cri[i].rank,&cri[i].sr);
 	}
 	
@@ -25,9 +25,9 @@ void main()
 	for(i=0;i<n;i++){
 	    for(j=0;j<n-i-1;j++){
 	        if(strcmp(cri[j].name,cri[j+1].name)>0){
-	            strcmp(tem,cri[j].name);
-	            strcmp(cri[j].name,cri[j+1].name);
-	            strcmp(cri[j+1].name,tem);
+	            tem=cri[j];
+	            cri[j]=cri[j+1];
+	            cri[j+1]=tem;
 	        }
 	    }
 	}
